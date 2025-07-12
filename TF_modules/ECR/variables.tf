7c49c5fca0acc3ce7f9e24016eb641d252019607
maolopez <maurizio.school@gmail.com>
2025-07-12 18:17:27 -0400

    adding pipelines to deploy to ECR

---
+++ TF_modules/ECR/variables.tf
@@ -0,0 +1,9 @@
+variable "ecr_repo_name" {
+  description = "The name of the repository on ECR"
+  type        = string
+}
+
+variable "scan_on_push" {
+  description = "The name of the repository on ECR"
+  type        = bool
+}
